b35b517ef6676aeb5e4309427ccc5d8d2b8ff930
Matias Osorio <matiasosorio1999@gmail.com>
2022-12-07 11:04:17 -0300

    add express

--- index.mjs
+++ index.mjs
@@ -1,32 +1,11 @@
-import { Octokit } from "octokit"
+import express from 'express';
 
-const octokit = new Octokit({
-  auth: process.env.GITHUB_TOKEN
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
-try {
-  const result = await octokit.request("GET /search/repositories?q=javascript", {});
-
-  // issues de un proyecto
-  // const result = await octokit.request("GET /repos/{owner}/{repo}/issues", {
-  //   owner: "facebook",
-  //   repo: "react",
-  // });
-
-  const parsedResult = result.data.items.map(item => ({
-    name: item.name,
-    url: item.html_url,
-    description: item.description,
-    stars: item.stargazers_count,
-    forks: item.forks_count,
-    language: item.language,
-    license: item.license?.name,
-    created: item.created_at,
-    updated: item.updated_at,
-  }));
-
-  console.log(parsedResult.length, result.data.total_count);
-
-} catch (error) {
-  console.log(`Error! Status: ${error.status}. Message: ${error.response.data.message}`)
-}
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!');
+});
---
+++ octokitjs.mjs
@@ -0,0 +1,32 @@
+import { Octokit } from "octokit";
+
+export const octokit = new Octokit({
+  auth: process.env.GITHUB_TOKEN
+});
+
+try {
+  const result = await octokit.request("GET /search/repositories?q=javascript", {});
+
+  // issues de un proyecto
+  // const resultIssues = await octokit.request("GET /repos/{owner}/{repo}/issues", {
+  //   owner: "facebook",
+  //   repo: "react",
+  // });
+
+  const parsedResult = result.data.items.map(item => ({
+    name: item.name,
+    url: item.html_url,
+    description: item.description,
+    stars: item.stargazers_count,
+    forks: item.forks_count,
+    language: item.language,
+    license: item.license?.name,
+    created: item.created_at,
+    updated: item.updated_at,
+  }));
+
+  console.log(parsedResult.length, result.data.total_count);
+
+} catch (error) {
+  console.log(`Error! Status: ${error.status}. Message: ${error.response.data.message}`)
+}

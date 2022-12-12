82b3ad336ba9d4ace9608f2fc50d00a26575bbda
Matias Osorio <matiasosorio1999@gmail.com>
2022-12-12 15:35:46 -0300

    create some endpoints

--- src/controllers/project.controller.js
+++ src/controllers/project.controller.js
@@ -14,6 +14,18 @@ exports.get = async (req, res) => {
       id: result.data.id,
       name: result.data.name,
       fullName: result.data.full_name,
+      description: result.data.description,
+      logo: result.data.logo,
+      topics: result.data.topics,
+      language: result.data.language,
+      url: result.data.html_url,
+      stars: result.data.stargazers_count,
+      forks: result.data.forks_count,
+      issues: result.data.open_issues_count,
+      watchers: result.data.watchers_count,
+      license: result.data.license?.name,
+      created: result.data.created_at,
+      updated: result.data.updated_at,
     }
 
     res.status(200).json(data);
@@ -34,6 +46,57 @@ exports.getIssues = async (req, res) => {
 
 
     res.status(200).json(resultIssues);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+exports.getPulls = async (req, res) => {
+  try {
+    const owner = req.query.owner;
+    const repo = req.query.repo;
+
+    const result = await octokit.request("GET /repos/{owner}/{repo}/pulls", {
+      owner,
+      repo,
+    });
+
+
+    res.status(200).json(result);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+exports.getCommitCount = async (req, res) => {
+  try {
+    const owner = req.query.owner;
+    const repo = req.query.repo;
+
+    const result = await octokit.request("/repos/{owner}/{repo}/stats/participation", {
+      owner,
+      repo,
+    });
+
+
+    res.status(200).json(result);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+exports.getHealthCheck = async (req, res) => {
+  try {
+    const owner = req.query.owner;
+    const repo = req.query.repo;
+
+    const result = await octokit.request("/repos/{owner}/{repo}/community/profile", {
+      owner,
+      repo,
+    });
+
+
+    res.status(200).json(result);
   } catch (error) {
     console.log(error);
   }
--- src/index.js
+++ src/index.js
@@ -10,6 +10,12 @@ app.get('/project', projectController.get);
 
 app.get('/project/issues', projectController.getIssues);
 
+app.get('/project/pulls', projectController.getPulls);
+
+app.get('/project/commit-count', projectController.getCommitCount);
+
+app.get('/project/health-check', projectController.getHealthCheck);
+
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!');
 });
